d08385a77bcd020968fc271e033b083b2707a37a
Javier Ribó <elribonazo@gmail.com>
2025-06-28 12:20:34 +0200

    fix: custom domain for gh pages Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   output: 'export',
   trailingSlash: true,
-  ...(isProd && isGithubPages && {
-    basePath: '/identus-ofa',
-    assetPrefix: '/identus-ofa',
-  }),
   images: {
     unoptimized: true
   },
